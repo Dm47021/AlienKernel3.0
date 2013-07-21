41386b4700dd5c537484e636569ccc819e977433
dm47021 <dm47021@hotmail.com>
2013-07-21 13:40:09 -0400

    mm: writeback: every 500ms the writeback daemon kicks in and flushes … …the dirty pages into disk. Thats not very pleasant for performance and *maybe* battery. This patch disables the writeback function while the screen is on, and enables it back when the screen is off to flush all the dirty pages. In my real world usage testing I notice a slight bump in smoothness and performance while using the device. (franciscofranco)

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -34,6 +34,7 @@
 #include <linux/syscalls.h>
 #include <linux/buffer_head.h>
 #include <linux/pagevec.h>
+#include <linux/earlysuspend.h>
 
 /*
  * After a CPU has dirtied this many pages, balance_dirty_pages_ratelimited
@@ -100,7 +101,7 @@ unsigned long vm_dirty_bytes;
  */
 
 #if defined(CONFIG_ALIEN_4_SCHEDULER) && (CONFIG_ALIEN_4_KERNELIZER_TWEAKS)
-unsigned int dirty_writeback_interval = 20 * 100; /* centiseconds */
+unsigned int dirty_writeback_interval = 0; /* centiseconds */
 #else
 unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
 #endif
@@ -797,6 +798,21 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 	.next		= NULL,
 };
 
+static void dirty_early_suspend(struct early_suspend *handler)
+{
+  dirty_writeback_interval = 5 * 100;
+}
+
+static void dirty_late_resume(struct early_suspend *handler)
+{
+  dirty_writeback_interval = 0;
+}
+
+static struct early_suspend dirty_suspend = {
+  .suspend = dirty_early_suspend,
+  .resume = dirty_late_resume,
+};
+
 /*
  * Called early on to tune the page writeback dirty limits.
  *
@@ -818,6 +834,8 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 void __init page_writeback_init(void)
 {
 	int shift;
+
+        register_early_suspend(&dirty_suspend);
 
 	writeback_set_ratelimit();
 	register_cpu_notifier(&ratelimit_nb);
